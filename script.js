76fc799d46f090ca84323438db02fe3b8390ba4d
DevAndresMunoz <andresgabrielmunoz@gmail.com>
2024-10-25 16:44:10 -0600

    completed task 2

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ getStates().then(states => {
     
     let middleIndex = Math.floor(statesObject.states.length / 2); // Dynamically determine middle index for array
     const firstHalfOfStates = statesArray.slice(0, middleIndex); // Save first half of original array to new variable
+    const secondHalfOfStates = statesArray.slice(middleIndex); // Save second half of original array to new variable
 
     for (let i = 0; i < firstHalfOfStates.length; i++) {
         let entryKey = firstHalfOfStates[i].name;
@@ -20,6 +21,15 @@ getStates().then(states => {
         const keyString = JSON.stringify(entryKey);
         const valueString = JSON.stringify(entryValue);
         localStorage.setItem(keyString, valueString);
+    }
+
+    for (let i = 0; i < secondHalfOfStates.length; i++) {
+        let entryKey = firstHalfOfStates[i].capital;
+        delete secondHalfOfStates[i].capital;
+        let entryValue = secondHalfOfStates[i];
+        const keyString = JSON.stringify(entryKey).toLowerCase();
+        const valueString = JSON.stringify(entryValue).toLowerCase();
+        sessionStorage.setItem(keyString, valueString);
     }
 });
 
